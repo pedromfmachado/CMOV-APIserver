f4968179d65011e16008c7dcbd199530a1ac2f97
FilipeCarvalho <filipemabpcarvalho@gmail.com>
2012-11-05 10:15:18 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CMOVApiserver::Application.config.secret_token = '0973be2ff977b698c686b067a0d24c919db8886f08a0042dcb53861c502447fe690c70e5eb7180687871f59e54130d51633b1ba7d059eb22020928a9819e27be'
