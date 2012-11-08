d6fe4a25d3a0bc3950e20508c75d08e54a433c58
Pedro Machado <pedromfmachado@gmail.com>
2012-11-08 04:07:24 +0000

    corrections in api registration response

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -5,11 +5,11 @@ class Api::RegistrationsController < Api::BaseController
 
     user = User.new(params[:user])
     if user.save
-      render :json=>{user.as_json(:auth_token=>user.authentication_token, :email=>user.email), :success => true}
+      render :json=>{:success => true, :auth_token=>user.authentication_token, :email=>user.email}
       return
     else
       warden.custom_failure!
-      render :json=>{user.errors, :success => false}
+      render :json=>[user.errors, :success => false]
     end
   end
 end
