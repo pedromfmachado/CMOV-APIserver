22d3189b2cb33320ab2f9340da4a6bb8ae81522a
Pedro Machado <pedromfmachado@gmail.com>
2012-11-11 17:01:26 +0000

    routes correction

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ CMOVApiserver::Application.routes.draw do
     get 'reservations/show/:id' => 'reservations#show'
     get 'reservations/get_trips'
     post 'reservations' => 'reservations#create'
-    post 'reservations/cancel' => 'reservations#cancel'
+    post 'reservations/cancel/:id' => 'reservations#cancel'
 
   end  
 
