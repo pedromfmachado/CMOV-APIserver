e7541f99c8af477e29830199e037390fb317c08f
Pedro Machado <pedromfmachado@gmail.com>
2012-11-12 21:30:09 +0000

    reservations api controller refactored

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -17,7 +17,7 @@ class Api::ReservationsController < Api::BaseController
       result = Array.new
       reservations.each do |r|
 
-        reservationTrips = Array.new
+        reservationTr+ips = Array.new
         ReservationTrip.where(:reservation_id => r.id).each do |rt|
 
           departureStation = Station.find(r.departureStation_id).name
@@ -60,6 +60,8 @@ class Api::ReservationsController < Api::BaseController
 
   end
 
+
+  # cancel reservation
   def cancel
 
     user = User.find_by_authentication_token(params[:token])
@@ -84,6 +86,7 @@ class Api::ReservationsController < Api::BaseController
 
   end
 
+  # create reservation
   def create
 
     user = User.find_by_authentication_token(params[:token])
@@ -122,7 +125,7 @@ class Api::ReservationsController < Api::BaseController
 
 		        for i in 0..(result.count-2)
 
-			        trip = getNextTrip(result[i], result[i+1], time)
+			        trip = getNextTrip(result[i], result[i+1], time, date)
 
               if trip == nil
                 render :json => { :success => false, :errors => ["Trip" => "Trip is not valid"]}
@@ -155,67 +158,80 @@ class Api::ReservationsController < Api::BaseController
 
     departureStation_id = params[:departureStation_id].to_i
     arrivalStation_id = params[:arrivalStation_id].to_i
-    time = Time.parse(params[:time]).change(:month => 1, :day => 1, :year => 2000)
     date = Date.parse(params[:date])
+    time = Time.parse(params[:time]).change(:month => 1, :day => 1, :year => 2000)
 
+    # calculating path
     result = Array.new
-    trips_array = makeReservations( departureStation_id, arrivalStation_id, Array.new, result)
-
+    makeReservations( departureStation_id, arrivalStation_id, Array.new, result)
+    # adding first station of reservation to array
     result <<  departureStation_id.to_i
-
     result.reverse!
 
     trips = Array.new
     for i in 0..(result.count-2)
 
-      trip = getNextTrip(result[i], result[i+1], time)
+      trip = getNextTrip(result[i], result[i+1], time, date)
 
       if trip == nil
-        render :json => []
+        render :json => { :success => false, :error => ["Trip" => "There are no trips available"]}
         return
       end
 
+      lotation = get_trip_lotation(trip,date)
+      
+      # if there the trip is full
+      if lotation <= 0
+        render :json => { :success => false, :error => ["Trip" => "All trips are full"]}
+        return
+      end      
+
+      # time in the departure station of segment
       time = getStationTime(trip, result[i])
 
       departure = Station.find(result[i]).name
       arrival = Station.find(result[i+1]).name
 
-      trips << { :trip => trip, :departure => departure, :arrival => arrival, :time => time.strftime('%H:%M') }
+      trips << { :trip => trip, :departure => departure, :arrival => arrival, :lotation => lotation, :time => time.strftime('%H:%M') }
 
     end
 
-    render :json =>  trips
+    render :json => { :success => true, :trips => trips }
 
   end
 
   private
-  def getNextTrip(departureStation_id, arrivalStation_id, actual_time)
+  def getNextTrip(departureStation_id, arrivalStation_id, actual_time, date)
 
+    # all lines to which a station belongs
     departureLines = LineStation.where(:station_id => departureStation_id).map { |x| x.line_id }
     arrivalLines = LineStation.where(:station_id => arrivalStation_id).map { |x| x.line_id }
 
+    # line that belongs to both of the stations
     line_id = (departureLines & arrivalLines).first
 
+    # order of the stations in line
     departureOrder = LineStation.where(:station_id => departureStation_id, :line_id => line_id).first.order
     arrivalOrder = LineStation.where(:station_id => arrivalStation_id, :line_id => line_id).first.order
 
-    puts 'time'
-    puts actual_time
+    # all the next trips
     trips = Trip.where('line_id = ? AND "beginTime" >= ?', line_id, actual_time).order('"beginTime" ASC')
-
+    
     if trips.empty?
       trips = Trip.where('line_id = ? AND "beginTime" >= ?', line_id, Time.gm(actual_time.year, actual_time.month, actual_time.day)).order('"beginTime" ASC')
     end
 
     trips.each do |trip|
 
+      # order of arrival and departure stations of the trip
       tripDepartureOrder = LineStation.where(:station_id => trip.departureStation_id, :line_id => trip.line_id).first.order
       tripArrivalOrder = LineStation.where(:station_id => trip.arrivalStation_id, :line_id => trip.line_id).first.order
 
-      if ((tripDepartureOrder <= tripArrivalOrder && departureOrder <= arrivalOrder)||
-           tripDepartureOrder >= tripArrivalOrder && departureOrder >= arrivalOrder)
+      # if stations are in the correct order
+      if (((tripDepartureOrder <= tripArrivalOrder && departureOrder <= arrivalOrder)||
+           (tripDepartureOrder >= tripArrivalOrder && departureOrder >= arrivalOrder)) &&
+           get_trip_lotation(trip,date))
 
-        #actual_time = getStationTime(trip, departureStation_id)
         return trip
 
       end
@@ -337,7 +353,7 @@ class Api::ReservationsController < Api::BaseController
 
     end
 
-    return train.lotation - count 
+    return train.maxCapacity - count 
 
   end
 
