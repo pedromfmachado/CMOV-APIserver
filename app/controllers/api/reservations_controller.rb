39516d01190ee36747b668f233ecd2a190072608
Pedro Machado <pedromfmachado@gmail.com>
2012-11-13 15:51:27 +0000

    corrected reservations index response

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -22,8 +22,8 @@ class Api::ReservationsController < Api::BaseController
 
         departureStation = Station.find(rt.departureStation_id).name
         arrivalStation = Station.find(rt.arrivalStation_id).name
-        reservationTrips << { :reservation_trip => rt, :departure => departureStation,
-                              :arrival => arrivalStation, :time => rt.time.strftime('%H:%M') }            
+        reservationTrips << { :reservation_trip => rt, :departure => departureStation_id,
+                              :arrival => arrivalStation_id, :time => rt.time.strftime('%H:%M') }            
 
         end
 
