c52dd516e1d092bca2e0bdae0ff6a3fd42cca235
Pedro Machado <pedromfmachado@gmail.com>
2012-11-15 23:04:30 +0000

    paid can now be mass assigned

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation < ActiveRecord::Base
   belongs_to :user
   belongs_to :station
-  attr_accessible :uuid, :user_id, :departureStation_id, :arrivalStation_id, :date, :canceled
+  attr_accessible :uuid, :user_id, :departureStation_id, :arrivalStation_id, :date, :canceled, :paid
 end
