d677c8f1c2d0f5a70ca8c8aed8d1573f7198a0ae
FilipeCarvalho <filipemabpcarvalho@gmail.com>
2012-11-19 10:53:42 +0000

    correction in uuid generation

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -18,7 +18,7 @@ class Api::BaseController < ApplicationController
         
         if (1.day.from_now >= time  )
 
-          if(r.paid)
+          if(r.paid && uuid == nil)
           
             uuid = UUIDTools::UUID.timestamp_create.to_s
             r.update_attributes(:uuid => uuid)
